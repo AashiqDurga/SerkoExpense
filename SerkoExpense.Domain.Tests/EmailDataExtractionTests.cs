9551b0d8181dfb5c71e09c902d83bb2238c8eccf
Aashiq Durga <aashiq.durga@myob.com>
2019-01-14 20:42:05 +1300

    added test for extracting basic expense data from email content

---
+++ SerkoExpense.Domain.Tests/EmailDataExtractionTests.cs
@@ -0,0 +1,61 @@
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using Xunit;
+
+namespace SerkoExpense.Domain.Tests
+{
+    public class EmailDataExtractionTests
+    {
+        [Fact]
+        public void GivenAnEmailWhenProcessingTheContentThenExtractExpenseInformation()
+        {
+            var expected = new Expense(){CostCentre = "DEV002", Total = 1024.01m, PaymentMethod = "personal card"};
+            var email = @"Hi Yvaine,
+            Please create an expense claim for the below. Relevant details are marked up as
+                requested…
+                <expense><cost_centre>DEV002</cost_centre>
+                <total>1024.01</total><payment_method>personal card</payment_method>
+                </expense>
+                From: Ivan Castle
+            Sent: Friday, 16 February 2018 10:32 AM
+            To: Antoine Lloyd <Antoine.Lloyd@example.com>
+                Subject: test
+            Hi Antoine,
+                Please create a reservation at the <vendor>Viaduct Steakhouse</vendor> our
+                <description>development team’s project end celebration dinner</description> on
+                <date>Tuesday 27 April 2017</date>. We expect to arrive around
+            7.15pm. Approximately 12 people but I’ll confirm exact numbers closer to the day.
+                Regards,
+            Ivan";
+
+            var dataExtractor = new EmailDataExtractor();
+            var result = dataExtractor.Extract(email);
+            
+            Assert.Equal(expected.CostCentre, result.CostCentre);
+            Assert.Equal(expected.PaymentMethod, result.PaymentMethod);
+            Assert.Equal(expected.Total, result.Total);
+        }
+    }
+
+    public class EmailDataExtractor
+    {
+        public Expense Extract(string email)
+        {
+            var expenseData = Regex.Match(email, "<expense>.*</expense>", RegexOptions.Singleline).Value;
+
+            var expenseXml = XDocument.Parse(expenseData);
+            var costCentre = expenseXml.Root.Element("cost_centre").Value;
+            var total = decimal.Parse(expenseXml.Root.Element("total").Value);
+            var paymentMethod = expenseXml.Root.Element("payment_method").Value;
+
+            return new Expense() {CostCentre = costCentre, Total = total, PaymentMethod = paymentMethod};
+        }
+    }
+
+    public class Expense
+    {
+        public string CostCentre { get; set; }
+        public decimal Total { get; set; }
+        public string PaymentMethod { get; set; }
+    }
+}
