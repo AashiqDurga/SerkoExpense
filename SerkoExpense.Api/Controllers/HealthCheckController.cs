f252a63163d4b227c66e38aac744714f346bfd45
Aashiq Durga <aashiq.durga@myob.com>
2019-01-16 19:58:42 +1300

    added healthcheck endpoint

---
+++ SerkoExpense.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using SerkoExpense.Application;
+
+namespace SerkoExpense.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return new OkObjectResult("Api is up and running.");
+        }
+    }
+}
